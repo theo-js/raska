5ff88279e383d90647aae056262e5fe24323ed1c
Theo Bayenet <dev.js.theo@gmail.com>
2025-12-27 21:16:37 +0100

    Technical: enable versioning

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' });
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '', method: 0 }],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
   console.info(`Application is running on: ${await app.getUrl()}`);
