123a2a4122e23018f6fd5852d3c5aef5b8e09a0d
Theo Bayenet <dev.js.theo@gmail.com>
2025-12-27 21:18:13 +0100

    Feature: add find all users endpoint w/ mocked data

---
+++ apps/backend/src/api/domains/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+      },
+      {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'jane.smith@example.com',
+      },
+      {
+        id: 3,
+        name: 'Alice Johnson',
+        email: 'alice.johnson@example.com',
+      },
+    ];
+  }
+}
